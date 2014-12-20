5b301e23e1c8084e5cdc8ea8f04a23eb499fa63b
Paolo Mainardi <paolo@twinbit.it>
2014-12-20 17:37:35 +0100

    automatic update container from build: 1419093455

--- build_structure.sh
+++ build_structure.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-mkdir -p /data
-mkdir -p /data/var/www
-mkdir -p /data/var/log/nginx
-mkdir -p /data/var/log/mysql
-mkdir -p /data/var/log/php
+mkdir -p /data && \
+mkdir -p /data/ssh-keys && \
+mkdir -p /data/etc && \
+mkdir -p /data/var/www && \
+mkdir -p /data/var/apps/drush && \
+mkdir -p /data/var/cache/composer  && \
+mkdir -p /data/var/log/nginx  \
+mkdir -p /data/var/log/mysql && \
+mkdir -p /data/var/log/php && \
 mkdir -p /data/var/lib/mysql
-mkdir -p /data/cache/drush
+exec $@
